5fb6902fc1aff2282f58bd7f304653e853a97149
Felix Gbinda Wono <Felix.gbinda@gmail.com>
2025-08-10 14:38:21 +0100

    Fix Switch platform configuration and add C-only test to isolate C++ runtime issues

---
+++ Vita3K-Switch/src/main_c_only.c
@@ -0,0 +1,34 @@
+#include <switch.h>
+
+int main(int argc, char* argv[]) {
+    // Initialize Switch services
+    if (R_FAILED(smInitialize())) {
+        return -1;
+    }
+    
+    // Initialize video
+    if (R_FAILED(viInitialize(ViServiceType_Manager))) {
+        return -1;
+    }
+    
+    // Initialize input
+    if (R_FAILED(hidInitialize())) {
+        return -1;
+    }
+    
+    // Wait for input to exit
+    while (appletMainLoop()) {
+        hidScanInput();
+        if (hidKeysDown(CONTROLLER_P1_AUTO) & KEY_PLUS) {
+            break;
+        }
+        svcSleepThread(10000000); // 10ms
+    }
+    
+    // Cleanup
+    hidExit();
+    viExit();
+    smExit();
+    
+    return 0;
+}
